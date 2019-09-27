23cfbacdc8af724ea64fd39658df325dbe31b84d
David Dembner <43708973+ShinyMagikarpet@users.noreply.github.com>
2019-09-27 00:32:29 -0400

    Scene Transtioner

---
+++ Assets/Scripts/Network/NetworkController.cs
@@ -0,0 +1,23 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        //connect to server
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster() {
+        //base.OnConnectedToMaster();
+        Debug.Log("You have connected to " + PhotonNetwork.CloudRegion + " server");
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI/ClickToGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ClickToGame : MonoBehaviour
+{
+
+    public void Find_Game() {
+        Debug.Log("Switching scenes");
+        SceneManager.LoadScene("Debug");
+    }
+    
+}
