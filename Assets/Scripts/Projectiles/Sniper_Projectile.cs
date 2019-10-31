4c6c37926bb7b24eaa5ab61b712d2e78cfb6d2e3
David Dembner <daviddembner@davids-mbp.resource.campus.njit.edu>
2019-10-31 12:35:58 -0400

    added sniper projectile

---
+++ Assets/Scripts/Projectiles/Sniper_Projectile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sniper_Projectile : Projectile{
+    public Sniper_Projectile(){
+        mName = "Bubble";
+        mSpeed = 1000.0f;
+        mTTL = 1.7f;
+        mDamage = 50.0f;
+    }
+}
