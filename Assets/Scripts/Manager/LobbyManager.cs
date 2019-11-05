a4ad1c496864228e04a4d1641c8061467bfd9e5f
David Dembner <43708973+ShinyMagikarpet@users.noreply.github.com>
2019-11-05 01:02:34 -0500

    player names now show on master client

--- Assets/Scripts/Manager/LobbyManager.cs
+++ Assets/Scripts/Manager/LobbyManager.cs
@@ -10,6 +10,13 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
     public Text[] team1 = new Text[4];
     public Text[] team2  = new Text[4];
+    private bool team1turn = false;
+
+    private void Start() {
+        if (PhotonNetwork.IsMasterClient) {
+            team1[0].text = PhotonNetwork.NickName;
+        }
+    }
 
     // Update is called once per frame
     void Update()
@@ -17,8 +24,28 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         
     }
 
+
+
+
     public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer) {
-        
+        if (team1turn) {
+            foreach(Text text in team1) {
+                if(text.text.ToLower().Equals("looking for player...")) {
+                    text.text = newPlayer.NickName;
+                    team1turn = false;
+                    break;
+                }
+            }
+        }
+        else {
+            foreach (Text text in team2) {
+                if (text.text.ToLower().Equals("looking for player...")) {
+                    text.text = newPlayer.NickName;
+                    team1turn = true;
+                    break;
+                }
+            }
+        }
     }
 
 }
