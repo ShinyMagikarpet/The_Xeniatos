7f36b6a630c720b8b39d701baee1a023cb918b18
David Dembner <davidtdembner@gmail.com>
2019-12-16 13:45:02 -0500

    adding teleporter

---
+++ Assets/Scripts/Terrain Obstacles/Teleporter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour{
+
+    public void OnControllerColliderHit(ControllerColliderHit hit) {
+        if (hit.collider.CompareTag("Weeb")) {
+            Debug.Log("You stepped on me collider");
+        }
+        else {
+            Debug.Log("Hello");
+        }
+    }
+
+    public void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Weeb")) {
+            Debug.Log("You stepped on me trigger");
+        }
+    }
+}
