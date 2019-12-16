6deb90a021ea01aa878f96ce2f3fc5759c23aaab
David Dembner <davidtdembner@gmail.com>
2019-12-16 15:12:16 -0500

    teleports now work with a cooldown

--- Assets/Scripts/Terrain Obstacles/Teleporter.cs
+++ Assets/Scripts/Terrain Obstacles/Teleporter.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class Teleporter : MonoBehaviour{
 
-    public void OnControllerColliderHit(ControllerColliderHit hit) {
-        if (hit.collider.CompareTag("Weeb")) {
-            Debug.Log("You stepped on me collider");
-        }
-        else {
-            Debug.Log("Hello");
+    public Teleporter linkedTeleporter;
+    private float cooldown = 0.0f;
+    private float cooldownRate = 5.0f;
+    private bool isOnCooldown = false;
+    private float hackedCooldownRate = 60.0f;
+    private bool isHacked = false;
+
+    public void OnTriggerStay(Collider other) {
+
+        if (isOnCooldown) return;
+
+        if (other.CompareTag("Weeb") || other.CompareTag("Player")) {
+            linkedTeleporter.isOnCooldown = true;
+            other.GetComponent<CharacterController>().enabled = false;
+            other.transform.position = linkedTeleporter.transform.position;
+            other.GetComponent<CharacterController>().enabled = true;
+            isOnCooldown = true;
+            StartCoroutine(TeleportCooldown(cooldownRate));
+
         }
     }
 
-    public void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Weeb")) {
-            Debug.Log("You stepped on me trigger");
-        }
+    IEnumerator TeleportCooldown(float timer) {
+        yield return new WaitForSeconds(timer);
+        isOnCooldown = false;
+        linkedTeleporter.isOnCooldown = false;
     }
 }
