6c9fd0e7e48019f7cfd8523f72eb2c98989bcfcf
David Dembner <43708973+ShinyMagikarpet@users.noreply.github.com>
2019-12-19 20:25:10 -0500

    fixed bug for weeb teleporter

--- Assets/Scripts/Terrain Obstacles/Teleporter.cs
+++ Assets/Scripts/Terrain Obstacles/Teleporter.cs
@@ -5,36 +5,44 @@ using UnityEngine;
 public class Teleporter : MonoBehaviour{
 
     public Teleporter linkedTeleporter;
-    private float cooldownRate = 5.0f;
-    private bool isOnCooldown = false;
+    private float cooldownRate = 15.0f;
+    private bool isOnCooldownWeeb = false;
+    private bool isOnCooldownPlayer = false;
     public int ID;
     public int linkedID = -1;
 
     public void OnTriggerEnter(Collider other) {
 
-        if (isOnCooldown) return;
-
-        if (other.CompareTag("Weeb")) {
+        if (other.CompareTag("Weeb") && !isOnCooldownWeeb) {
+            linkedTeleporter.isOnCooldownWeeb = true;
             other.GetComponent<CharacterController>().enabled = false;
             other.transform.position = linkedTeleporter.transform.position;
             other.GetComponent<CharacterController>().enabled = true;
+            isOnCooldownWeeb = true;
+            StartCoroutine(TeleportCooldownWeeb(cooldownRate * 0.1f));
         }
 
-        if (other.CompareTag("Player")) {
-            linkedTeleporter.isOnCooldown = true;
+        if (other.CompareTag("Player") && !isOnCooldownPlayer) {
+            linkedTeleporter.isOnCooldownPlayer = true;
             other.GetComponent<CharacterController>().enabled = false;
             other.transform.position = linkedTeleporter.transform.position;
             other.GetComponent<CharacterController>().enabled = true;
-            isOnCooldown = true;
-            StartCoroutine(TeleportCooldown(cooldownRate));
+            isOnCooldownPlayer = true;
+            StartCoroutine(TeleportCooldownPlayer(cooldownRate));
 
         }
     }
 
-    IEnumerator TeleportCooldown(float timer) {
+    IEnumerator TeleportCooldownWeeb(float timer) {
         yield return new WaitForSeconds(timer);
-        isOnCooldown = false;
-        linkedTeleporter.isOnCooldown = false;
+        isOnCooldownWeeb = false;
+        linkedTeleporter.isOnCooldownWeeb = false;
+    }
+
+    IEnumerator TeleportCooldownPlayer(float timer) {
+        yield return new WaitForSeconds(timer);
+        isOnCooldownWeeb = false;
+        linkedTeleporter.isOnCooldownWeeb = false;
     }
 
     public int Get_Linked_ID() {
